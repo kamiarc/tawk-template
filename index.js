dd6a36d8d727382947abf2dccf6a9fffbcd5fcbb
Lucas Zambianchi <lucas@onesight.com.br>
2020-09-16 12:22:03 +0200

    Adding tawkid var

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
-    s1.src='https://embed.tawk.to/5f60ea714704467e89ef30fe/default';
+    // s1.src='https://embed.tawk.to/5f60ea714704467e89ef30fe/default';
+    s1.src='https://embed.tawk.to/'+data.tawkid+'/default';
     s1.setAttribute('crossorigin','*');
     s1.setAttribute('defer',true);
     s0.parentNode.insertBefore(s1,s0);
